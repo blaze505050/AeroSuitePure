db9a2bcdfc2dde0bb97bc61f0a6a0a4c8d3f49df
Blaze <suvvariaditya3@gmail.com>
2026-03-25 13:47:41 +0530

    Delete tests directory

--- tests/test_panel_solver.py
+++
@@ -1,9 +0,0 @@
-from core.airfoil_utils import naca4_to_coords
-from core.panel_solver import panel_method_cl
-
-def test_panel_solver_runs():
-    x,y = naca4_to_coords('0012', n=60)
-    cl, info = panel_method_cl(x, y, 2.0)
-    assert isinstance(cl, float)
-    assert 'Cp' in info
-    assert len(info['Cp']) == len(x)-1
